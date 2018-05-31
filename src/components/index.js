8a2f295b6690462d2f2d071d2c9010cfd72a92b7
Mat Ruane <mat@ruane.net.nz>
2018-05-31 21:10:30 +1200

    wiped for new attempt

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import App from './App'
-import Board from './Board'
-import Square from './Square'
-
-export { App, Board, Square }
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-import { App } from './components'
-import registerServiceWorker from './registerServiceWorker'
-
-render(<App />, document.getElementById('root'))
-registerServiceWorker()
